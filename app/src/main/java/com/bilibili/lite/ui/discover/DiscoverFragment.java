523a598b4bbb0ef74093b9330ec10b433c8bd254
yedan2952 <yedan2952@users.noreply.github.com>
2026-07-08 11:17:42 +0800

    fix: remove categoryLive reference from DiscoverFragment (missing in layout)

--- app/src/main/java/com/bilibili/lite/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/bilibili/lite/ui/discover/DiscoverFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,7 +12,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bilibili.lite.R;
-import com.bilibili.lite.data.model.VideoInfo;
 import com.bilibili.lite.ui.home.VideoFeedAdapter;
 import com.bilibili.lite.ui.search.SearchActivity;
 import com.bilibili.lite.ui.video.VideoDetailActivity;
@@ -48,9 +45,6 @@ public class DiscoverFragment extends Fragment {
 
         view.findViewById(R.id.searchBar).setOnClickListener(v ->
                 startActivity(new Intent(getActivity(), SearchActivity.class)));
-
-        TextView categoryLive = view.findViewById(R.id.categoryLive);
-        categoryLive.setOnClickListener(v -> Toast.makeText(getContext(), "Live - coming soon", Toast.LENGTH_SHORT).show());
 
         viewModel = new ViewModelProvider(this).get(DiscoverViewModel.class);
         viewModel.trending.observe(getViewLifecycleOwner(), videos -> adapter.submitList(videos));
